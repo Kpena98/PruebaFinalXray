0bc70dc5f26f2c89fe013b0c7b5dbdd75cf93301
Kpena98 <82413094+Kpena98@users.noreply.github.com>
2022-05-02 15:20:04 -0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
- 
+projectName=$1
+reportFile=$2
+varDate=$(date +%c)
+rm $reportFile
 JMETERPLUGINSCMD=JMeterPluginsCMD.sh
-
-
 # run jmeter and produce a JTL csv report
-jmeter -n -t  $WORKSPACE/hb_0324.jmx -l results.jtl -e -o dashboard
+rm -R dashboard/
+jmeter -n -t  $WORKSPACE/$projectName -l $WORKSPACE/$reportFile -e -o dashboard
 
 # process JTL and covert it to a synthesis report as CSV
 $JMETERPLUGINSCMD --generate-csv synthesis_results.csv --input-jtl results.jtl --plugin-type SynthesisReport
@@ -12,3 +14,5 @@ $JMETERPLUGINSCMD --tool Reporter --generate-csv reports/aggregate_results.csv -
 
 $JMETERPLUGINSCMD --generate-png reports/ResponseTimesOverTime.png --input-jtl results.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600
 $JMETERPLUGINSCMD --generate-png reports/TransactionsPerSecond.png --input-jtl results.jtl --plugin-type TransactionsPerSecond --width 800 --height 600
+java -jar $WORKSPACE/jmeter-junit-xml-converter-0.0.1-SNAPSHOT-jar-with-dependencies.jar results.jtl reporteconvertido.xml
+curl -D- -X POST "https://laboratoriobfcl.atlassian.net/rest/api/3/issue/$TESTPLAN/attachments" -H 'X-Atlassian-Token: no-check' -F "file="@$WORKSPACE/alternate_reporteconvertido.xml"" -H 'application/octet-stream' -H 'Authorization: Basic a2V2aW4ucGVuYUB0c29mdGxhdGFtLmNvbTp5eVZDWWNGU2dPMkRVbk5KN3R4RTA3MjU= ' 
